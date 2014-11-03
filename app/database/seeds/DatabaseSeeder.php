587c898e4feac4179ed31e1bed945d31d3ed65ef
shotechs <shotechs@gmail.com>
2014-11-03 12:30:27 -0700

    start

---
+++ app/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+public function run()
+{
+    Eloquent::unguard();
+ 
+    // Add or Uncomment this line
+    $this->call('UserTableSeeder');
+}
+}
